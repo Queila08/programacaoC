266e8d3542c8702030d18ba089defeaff79779e9
Queila08 <menequeila8@outlook.com>
2025-06-04 22:43:55 +0000

    teste

--- Untitled-4.cpp
+++ Untitled-4.cpp
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
-int main() {
+int main () {
+    
+    float temperatura; umidade;
+    unsigned int estoque; estoque_minimo = 1000
 
-    float temperatura = 25.0;
-    if (temperatura > 30.00) {
-        printf("está calor\n");
-    }else {
-        printf(" não está calor\n");    
-  
+    printf("Entre com a temperatura: \n");
+    scanf("%f", &temperatura);
+    printf("Entre com a umidade: \n");
+    scanf("%f", &umidade);
+    printf("Entre com o estoque: \n");
+    scanf("%u", &estoque);
+
+    if( temperatura > 30){
+        printf("Temperatura está alta.\n");
+    } else {
+        printf("Temperatura está dentro dos parâmetros.\n");
+    }
+    if (umidade > 50); {
+        printf("umidade está muito elevada.\n");
+    } else {
+        printf("umidade está dentro dos parâmetros.\n");    
+    }
+    
+    if (estoque < estoque_minimo){
+        printf("Estoque abaixo do minimo\n");
+    } else {
+        printf("estoque normal!\n");
+    }
+
+    
 }
-
