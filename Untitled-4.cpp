a1b56c7cdd9a76e52b28f6c5ee9eea55271538cc
Queila08 <menequeila8@outlook.com>
2025-06-04 21:38:34 +0000

    teste

--- Untitled-4.cpp
+++ Untitled-4.cpp
@@ -2,5 +2,11 @@
 
 int main() {
 
-    
+    float temperatura = 25.0;
+    if (temperatura > 30.00) {
+        printf("está calor\n");
+    }else {
+        printf(" não está calor\n");    
+  
 }
+
